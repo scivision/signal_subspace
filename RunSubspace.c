5e940bc6a02d1e92b187be1e470cbd23e8b5aa36
scivision <scivision@users.noreply.github.com>
2016-03-14 22:55:52 -0400

    pass by reference array

--- RunSubspace.c
+++ RunSubspace.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <complex.h>
 
-extern void __subspace_MOD_esprittone(complex double [], int *,int *,int *, double *, double *);
+extern void __subspace_MOD_esprit(complex double [], int *, int *, int *, double *, double [], double []);
 
 int main () {
  int N=12;
@@ -12,12 +12,14 @@ int main () {
 // this is a noisy 21234.5 Hz sinusoid, extreme case where we have very few samples to build up the noise estimate. Normally you would use say 10-100x as many samples. 
  double complex x[12]={1.0004620 - I*0.0008895,  -0.9338329 + I*0.0004178,  0.7494963 + I*0.0010474,  -0.4654616 + I*0.0008352,  0.1242105 - I*0.0000312,   0.2369377 + I*0.0013630,-0.5662766 + I*0.0000239,   0.8222365 - I*0.0004521,  -0.9696022 - I*0.0004094, 0.9948683 + I*0.0004826,  -0.8875562 + I*0.0003774,   0.6680975 - I*0.0008353};
 
- double tone;
+//---------------------------------
+double tones[L], sigma[L];
 
+// if we pass the reference to the first array address, the rest of the array will follow (tones,sigma)
+__subspace_MOD_esprit(&x[0], &N, &L, &M, &fs, &tones[0], &sigma[0]);
 
-__subspace_MOD_esprittone(x, &N, &L, &M, &fs, &tone);
+printf("tones: %f %f\n",tones[0],tones[1]);
+printf("sigma: %f %f",sigma[0],sigma[1]);
 
-// Estimate has < 1% error. 
- printf("%f",tone);
 }
 
