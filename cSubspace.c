a0f0f5326e864c2baed9e8cc22930dbbee4f3bb8
scivision <scivision@users.noreply.github.com>
2016-04-18 00:22:41 -0400

    harmonize C / C++

--- cSubspace.c
+++ cSubspace.c
@@ -2,46 +2,52 @@
 #include <stdlib.h>
 #include <math.h>
 
-extern void __subspace_MOD_esprit(float [], const int *, const int *, int *, const float *, float [], float []);
+extern void __signals_MOD_signoise(const float*, const float*, const float*, int*, float []);
 
-extern void __signals_MOD_signoise(const float *,const float *, const float *, const int *, float []);
+extern void __filters_MOD_fircircfilter(float [], int*,float [],int*,float [], int*);
+
+extern void __subspace_MOD_esprit(float [], int*, int*, int*, const float*, float [], float []);
+
 
 int main () {
- const int N=1024;
- const int L=4;
- int M=N/2;
- const float fs=48000.;
+printf("ANSI C Esprit\n");
+
+int Ns=1024;
+int Ntone=2;
+int M=Ns/2;
+const float fs=48000.;
 //------- noisy signal generation ----------------
- const float snr = 60.0; // dB, arbitrary
- const float f0 = (float)12345.6; //arbitrary
- 
- float * x;
- x = malloc(N*sizeof(float));
- __signals_MOD_signoise(&fs, &f0, &snr, &N, &x[0]);
+const float snr = 60.; // dB, arbitrary
+const float f0 = (float)12345.6; //arbitrary
+
+float * x;
+x = malloc(Ns*sizeof(float));
+__signals_MOD_signoise(&fs, &f0, &snr, &Ns, &x[0]);
 
 //---- signal estimation -----------------------------
-float * tones; 
-float * sigma;
-tones = malloc(L*sizeof(float));
-sigma = malloc(L*sizeof(float));
+float * tones = malloc(Ntone*sizeof(float));
+float * sigma = malloc(Ntone*sizeof(float));
 
 // if we pass the reference to the first array address, the rest of the array will follow (tones,sigma)
-__subspace_MOD_esprit(&x[0], &N, &L, &M, &fs, &tones[0], &sigma[0]);
+__subspace_MOD_esprit(&x[0], &Ns, &Ntone, &M, &fs, &tones[0], &sigma[0]);
 
 free(x);
 
-printf("ANSI C Esprit\n");
-printf("tones: %f %f\n",tones[0],tones[1]);
-printf("sigma: %f %f\n",sigma[0],sigma[1]);
+printf("estimated tone freq [Hz]: ");
+for (int i=0; i<Ntone; i++) printf("%f ",tones[i]);
+
+printf("\nwith sigma:               ");
+for (int i=0; i<Ntone; i++) printf("%f ",sigma[i]);
+printf("\n");
 
 if (fabsf(tones[0]-f0)>0.0001*f0){
-    perror("failed to meet tolerance\n");
-    exit(EXIT_FAILURE);
+    fprintf(stderr,"E: failed to meet tolerance\n");
+    return EXIT_FAILURE;
 }
 else{
     printf("OK\n");
-    exit(EXIT_SUCCESS);
+    return EXIT_SUCCESS;
 }
 
-}
+} // main
 
