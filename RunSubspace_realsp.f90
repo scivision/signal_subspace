7397a07550bb6ee165d9261617ecb0f119fdbb44
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-02-25 11:20:31 -0500

    python not working for Fortran, have to pick one or the other, or build two programs after all?

--- RunSubspace_realsp.f90
+++ RunSubspace_realsp.f90
@@ -34,19 +34,19 @@ M = Ns / 2_c_int
 narg = command_argument_count()
 
 if (narg > 0) then
-    call get_command_argument(1,arg); read(arg,*) Ns
+  call get_command_argument(1,arg); read(arg,*) Ns
 endif
 if (narg > 1) then
-    call get_command_argument(2,arg); read(arg,*) fs
+  call get_command_argument(2,arg); read(arg,*) fs
 endif
 if (narg > 2) then
-    call get_command_argument(3,arg); read(arg,*) Ntone
+  call get_command_argument(3,arg); read(arg,*) Ntone
 endif
 if (narg > 3) then
-    call get_command_argument(4,arg); read(arg,*) M
+  call get_command_argument(4,arg); read(arg,*) M
 endif
 if (narg > 4) then
-    call get_command_argument(5,arg); read(arg,*) snr !dB
+  call get_command_argument(5,arg); read(arg,*) snr !dB
 endif
 
 print *, "Fortran Esprit: Real Single Precision"
@@ -54,8 +54,8 @@ print *, "Fortran Esprit: Real Single Precision"
 allocate(x(Ns), y(Ns), tones(Ntone/2), sigma(Ntone/2))
 !--- checking system numerics --------------
 if (storage_size(fs) /= 32) then
-    write(stderr,*) 'expected 32-bit real but you have real bits: ', storage_size(fs)
-    error stop
+  write(stderr,*) 'expected 32-bit real but you have real bits: ', storage_size(fs)
+  error stop
 endif
 !------ simulate noisy signal ------------ 
 call signoise(fs,f0,snr,Ns,&
@@ -64,22 +64,22 @@ call signoise(fs,f0,snr,Ns,&
 ! read coefficients 'b'
 open (newunit=u, file=bfn, status='old', action='read', iostat=fstat)
 if (fstat == 0) then
-    read(u,*) Nb !first line of file: number of coeff
-    allocate(b(Nb))
-    read(u,*) b ! second line all coeff
-    close(u)
+  read(u,*) Nb !first line of file: number of coeff
+  allocate(b(Nb))
+  read(u,*) b ! second line all coeff
+  close(u)
 !    print *,'FIR Coefficients'
 !    print '(EN10.1)', b
 
-    call system_clock(tic)
-    call fircircfilter(x, size(x,kind=c_int), b, size(b,kind=c_int), y)
-    call system_clock(toc)
-    print '(A,EN10.1)', 'seconds to FIR filter: ',sysclock2ms(toc-tic)/1000
+  call system_clock(tic)
+  call fircircfilter(x, size(x,kind=c_int), b, size(b,kind=c_int), y)
+  call system_clock(toc)
+  print '(A,EN10.1)', 'seconds to FIR filter: ',sysclock2ms(toc-tic)/1000
 endif
 
 if (fstat /= 0 .or. ieee_is_nan(y(1))) then
-    write(stderr,*) 'skipped FIR filter.'
-    y=x
+  write(stderr,*) 'skipped FIR filter.'
+  y=x
 endif
 !------ estimate frequency of sinusoid in noise --------
 call system_clock(tic)
@@ -98,6 +98,4 @@ print *,'OK'
 
 ! deallocate(x,y,tones,sigma)  ! this is automatic going out of scope
 end program test_subspace
-
-
 
--- perf.f90
+++ perf.f90
@@ -1,24 +1,25 @@
 ! https://github.com/JuliaLang/julia/blob/master/test/perf/micro/perf.f90
 module perf
-    use, intrinsic:: iso_fortran_env, only: int64, real64
-    use, intrinsic:: iso_c_binding, only: c_bool
-    
-    implicit none
-    private
-    public:: sysclock2ms
+  use, intrinsic:: iso_fortran_env, only: int64, real64
+  use, intrinsic:: iso_c_binding, only: c_bool
+  
+  implicit none
+  private
+  public:: sysclock2ms
+  
 contains
 
-    real(real64) function sysclock2ms(t)
-    ! Convert a number of clock ticks, as returned by system_clock called
-    ! with integer(i64) arguments, to milliseconds
+real(real64) function sysclock2ms(t)
+! Convert a number of clock ticks, as returned by system_clock called
+! with integer(i64) arguments, to milliseconds
 
-        integer(int64), intent(in) :: t
-        integer(int64) :: rate
-        real(real64) ::  r
-        call system_clock(count_rate=rate)
-        r = 1000._real64 / rate
-        sysclock2ms = t * r
-    end function sysclock2ms
+  integer(int64), intent(in) :: t
+  integer(int64) :: rate
+  real(real64) ::  r
+  call system_clock(count_rate=rate)
+  r = 1000._real64 / rate
+  sysclock2ms = t * r
+end function sysclock2ms
 
 
 !    subroutine assert(cond)
