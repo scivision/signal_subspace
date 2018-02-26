0a5e3b3ea8191ea9dcb6c0059c017b858f811788
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-02-26 02:58:30 -0500

    typo

--- RunSubspace_realsp.f90
+++ RunSubspace_realsp.f90
@@ -56,7 +56,7 @@ endif
 
 print *, "Fortran Esprit: real bits:",storage_size(fs)
 !---------- assign variable size arrays ---------------
-allocate(x(Ns), y(Ns), tones(Ntone/4), sigma(Ntone/2))
+allocate(x(Ns), y(Ns), tones(Ntone/2), sigma(Ntone/2))
 
 !------ simulate noisy signal ------------ 
 call signoise(fs,f0,snr, x)
