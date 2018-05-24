58281c36be276038b6a65be156a38b5f16e99d2b
scivision <scivision@users.noreply.github.com>
2018-05-24 12:24:50 -0400

    init

---
+++ comm_legacy.f90
@@ -0,0 +1,28 @@
+module comm
+    use, intrinsic :: iso_c_binding, only: sp=>C_FLOAT, dp=>C_DOUBLE, int64=>C_LONG_LONG, sizeof=>c_sizeof
+    use, intrinsic :: iso_fortran_env, only: stderr=>error_unit
+    implicit none
+    
+    complex(dp),parameter :: J=(0._dp, 1._dp)
+    real(dp),parameter :: pi = 4._dp*atan(1._dp)
+
+contains
+
+subroutine init_random_seed()
+    integer :: i, n, clock
+    integer, allocatable :: seed(:)
+
+    call random_seed(size=n)
+    allocate(seed(n))
+    call system_clock(count=clock)
+    seed = clock + 37 * [ (i - 1, i = 1, n) ]
+    call random_seed(put=seed)
+end subroutine
+
+pure subroutine err(msg)
+  character(*), intent(in) :: msg
+  write(stderr,*) msg
+  stop -1
+end subroutine err
+
+end module comm
