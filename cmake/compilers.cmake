2385319ab99c1ecc20e474218e8269cefff192c8
Michael Hirsch <scivision@users.noreply.github.com>
2021-06-28 22:33:50 -0400

    FindLapack template

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,4 +1,6 @@
-if(CMAKE_Fortran_COMPILER_ID STREQUAL Intel)
+include(CheckSourceCompiles)
+
+if(CMAKE_Fortran_COMPILER_ID MATCHES Intel)
   if(WIN32)
     add_compile_options(/QxHost)
     string(APPEND CMAKE_Fortran_FLAGS " /warn:declarations /heap-arrays")
@@ -8,23 +10,16 @@ if(CMAKE_Fortran_COMPILER_ID STREQUAL Intel)
     string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -check all -fpe0 -warn -traceback -debug extended")
   endif()
 elseif(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
-  if(CMAKE_Fortran_COMPILER_VERSION VERSION_GREATER_EQUAL 8)
-    string(APPEND CMAKE_Fortran_FLAGS " -std=f2018")
-  endif()
-
   string(APPEND CMAKE_Fortran_FLAGS " -fimplicit-none -Werror=array-bounds")
   string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -ffpe-trap=zero,overflow,underflow")
   # mtune=native for better cross-platform
   add_compile_options(-mtune=native -Wall -Wextra)
 endif()
 
-if(PROJECT_SOURCE_DIR STREQUAL CMAKE_SOURCE_DIR)
-  include(CheckIPOSupported)
-  check_ipo_supported(RESULT lto_supported)
-  if(lto_supported)
-    set(CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE)
-  endif()
-endif()
 
-include(CheckFortranSourceCompiles)
-check_fortran_source_compiles("call random_init(.false., .false.); end" f18random SRC_EXT f90)
+
+check_source_compiles(Fortran
+  "program test
+  call random_init(.false., .false.)
+  end program"
+  f18random)
