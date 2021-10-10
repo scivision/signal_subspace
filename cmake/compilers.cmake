a5e38e0930886d801a6ba5df408af06b8f587e3a
Michael Hirsch <scivision@users.noreply.github.com>
2021-10-10 11:53:11 -0400

    cmake best practices compiler flags

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,19 +1,17 @@
 include(CheckSourceCompiles)
 
-if(CMAKE_Fortran_COMPILER_ID MATCHES Intel)
-  if(WIN32)
-    add_compile_options(/QxHost)
-    string(APPEND CMAKE_Fortran_FLAGS " /warn:declarations /heap-arrays")
-  else()
-    add_compile_options(-xHost)
-    string(APPEND CMAKE_Fortran_FLAGS " -warn declarations")
-    string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -check all -fpe0 -warn -traceback -debug extended")
-  endif()
+if(CMAKE_Fortran_COMPILER_ID MATCHES "^Intel")
+  add_compile_options(
+  $<IF:$<BOOL:${WIN32}>,/QxHost,-xHost>
+  "$<$<COMPILE_LANGUAGE:Fortran>:-traceback;-heap-arrays>"
+  "$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug>>:-check all;-fpe0;-warn;-traceback;-debug extended>"
+  )
 elseif(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
-  string(APPEND CMAKE_Fortran_FLAGS " -fimplicit-none -Werror=array-bounds")
-  string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -ffpe-trap=zero,overflow,underflow")
-  # mtune=native for better cross-platform
-  add_compile_options(-mtune=native -Wall -Wextra)
+  add_compile_options(
+  "$<$<COMPILE_LANGUAGE:Fortran>:-fimplicit-none;-Werror=array-bounds>"
+  "$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug>>:-ffpe-trap=zero,overflow,underflow>"
+  -mtune=native -Wall -Wextra
+  )
 endif()
 
 
